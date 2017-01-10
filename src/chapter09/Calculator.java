4d5aeb534dbf2707ad5a9660fb13a12af9328025
smallt-TAO <llt5662720@163.com>
2017-01-10 20:48:34 +0800

    Update the dome of calculator.

--- src/chapter09/Calculator.java
+++ src/chapter09/Calculator.java
@@ -7,3 +7,71 @@ import javax.swing.*;
 public class Calculator {
 
 }
+
+
+/**
+ * A frame with a calculator panel.
+ */
+class CalculatorFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CalculatorFrame() {
+		setTitle("Calculator");
+		CalculatorPanel panel = new CalculatorPanel();
+		add(panel);
+		pack();
+	}
+}
+
+
+/**
+ * A panel with calculator buttons and a result display.
+ */
+class CalculatorPanel extends JPanel {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JButton display;
+	private JPanel panel;
+	private double result;
+	private String lastCommand;
+	private boolean start;
+	
+	public CalculatorPanel() {
+		setLayout(new BorderLayout());
+		
+		result = 0;
+		lastCommand = "=";
+		start = true;
+		
+		// add the display
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
