ce83c4921479df53914501c51802253300bff7de
smallt-TAO <llt5662720@163.com>
2017-01-12 20:56:15 +0800

    Update the demo text Code of CheckBoxTest java.

---
+++ src/chapter09/CheckBoxTest.java
@@ -0,0 +1,75 @@
+package chapter09;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * @version 1.00
+ * @author Smalltao
+ *
+ */
+public class CheckBoxTest {
+    public static void main(String[] args) {
+    	EventQueue.invokeLater(new Runnable() {
+    		public void run() {
+    			CheckBoxFrame frame = new CheckBoxFrame();
+    			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    			frame.setVisible(true);
+    		}
+    	});
+    }
+}
+
+/**
+ * A frame with a sample text label and check boxes for selecting font attributes.
+ */
+class CheckBoxFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final int FONTSIZE = 12;
+	private JLabel label;
+	private JCheckBox bold;
+	private JCheckBox italic;
+	
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+	
+	public CheckBoxFrame() {
+		setTitle("CheckBoxTest");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		
+		// add the sample text label
+		label = new JLabel("The quick brown fox jumps over the lazy dog.");
+		label.setFont(new Font("Serif", Font.PLAIN, FONTSIZE));
+		add(label, BorderLayout.CENTER);
+		
+		// this listener sets the font attribute of 
+		// the label to the check box state
+		ActionListener listener = new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				int mode = 0;
+				if (bold.isSelected()) mode += Font.BOLD;
+				if (italic.isSelected()) mode += Font.ITALIC;
+				label.setFont(new Font("Serif", mode, FONTSIZE));;
+				
+			}
+		};
+		
+		// add the check boxes
+		JPanel buttonPanel = new JPanel();
+		
+		bold = new JCheckBox("Bold");
+		bold.addActionListener(listener);
+		buttonPanel.add(bold);
+		
+		italic = new JCheckBox("Italic");
+		italic.addActionListener(listener);
+		buttonPanel.add(italic);
+		
+		add(buttonPanel, BorderLayout.SOUTH);
+	}
+}
