3767fcc05c7f4db4eefcc80afbca676cbc7b8f74
smallt-TAO <llt5662720@163.com>
2017-01-30 22:47:29 +0800

    #20170130 Today, meet the girl friend at the town. At the same time, update the DataExchange test code.

---
+++ src/chapter09/DataExchangeTest.java
@@ -0,0 +1,45 @@
+package chapter09;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * @version 1.00
+ * @author Smalltao
+ *
+ */
+public class DataExchangeTest {
+
+}
+
+
+/**
+ * A user has a name and password. For security reasons, the password is stored as
+ * char[], not a String.
+ */
+class User {
+	private String name;
+	private char[] password;
+	
+	public User(String aName, char[] aPassword) {
+		name = aName;
+		password = aPassword;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public char[] getPassword() {
+		return password;
+	}
+	
+	public void setName(String aName) {
+		name = aName;
+	}
+	
+	public void setPassword(char[] aPassword) {
+		password = aPassword;
+	}
+}
