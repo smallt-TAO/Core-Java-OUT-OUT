4820b3a1524109d872b038d799da07d40c08dc62
smallt-TAO <llt5662720@163.com>
2017-02-04 22:23:44 +0800

    #20170204 Today update the GBC code, as the part of the GridLayout.

---
+++ src/chapter09/GBC.java
@@ -0,0 +1,51 @@
+package chapter09;
+
+import java.awt.*;
+
+/**
+ * This class simplifies the use of the GridBagConstraints class.
+ * @version 1.00
+ * @author Smalltao
+ *
+ */
+
+public class GBC extends GridBagConstraints{
+    public GBC(int gridx, int gridy) {
+    	this.gridx = gridx;
+    	this.gridy = gridy;
+    }
+    
+    public GBC(int gridx, int gridy, int gridwidth, int gridheight) {
+    	this.gridx = gridx;
+    	this.gridy = gridy;
+    	this.gridwidth = gridwidth;
+    	this.gridheight = gridheight;
+    }
+    
+    public GBC setAnchor(int anchor) {
+    	this.anchor = anchor;
+    	return this;
+    }
+    
+    public GBC setFill(int fill) {
+    	this.fill = fill;
+    	return this;
+    }
+    
+    public GBC setWeight(double weightx, double weighty) {
+    	this.weightx = weightx;
+    	this.weighty = weighty;
+    	return this;
+    }
+    
+    public GBC setInsets(int top, int left, int bottom, int right) {
+    	this.insets = new Insets(top, left, bottom, right);
+    	return this;
+    }
+    
+    public GBC setIpad(int ipadx, int ipady) {
+    	this.ipadx = ipadx;
+    	this.ipady = ipady;
+    	return this;
+    }
+}
