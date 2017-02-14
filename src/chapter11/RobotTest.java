492b01408364013f5aa02399215088a45b5a1de0
smallt-TAO <llt5662720@163.com>
2017-02-14 19:34:14 +0800

    #20170214 Valentine's Day

--- src/chapter11/RobotTest.java
+++ src/chapter11/RobotTest.java
@@ -11,7 +11,58 @@ import javax.swing.*;
  *
  */
 public class RobotTest {
-
+    public static void main(String[] args) {
+    	EventQueue.invokeLater(new Runnable() {
+    		public void run() {
+    			// make frame with a button panel
+    			ButtonFrame frame = new ButtonFrame();
+    			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    			frame.setVisible(true);
+    			
+    			// attach a robot to the screen device
+    			GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    			GraphicsDevice screen = environment.getDefaultScreenDevice();
+    			
+    			try {
+    				Robot robot = new Robot(screen);
+    				runTest(robot);
+    			} 
+    			catch (AWTException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	});
+    }
+    
+    /**
+     * Runs a sample test procsdure
+     * @param robot the robot attached to the screen device.
+     */
+    public static void runTest(Robot robot) {
+    	// simulate a space bar press
+    	robot.keyPress(' ');
+    	robot.keyRelease(' ');
+    	
+    	// simulate a tab key followed by a space
+    	robot.delay(20000);
+    	robot.keyPress(KeyEvent.VK_TAB);	 
+    	robot.keyRelease(KeyEvent.VK_TAB);
+    	robot.keyPress(' ');
+    	robot.keyRelease(' ');
+    	
+    	// simulate a mouse click over the rightmost button
+    	robot.delay(2000);
+    	robot.mouseMove(200, 50);
+    	robot.mousePress(InputEvent.BUTTON1_MASK);
+    	robot.mouseRelease(InputEvent.BUTTON1_MASK);
+    	
+    	// capture the screen and show the resulting image
+    	robot.delay(2000);
+    	BufferedImage image = robot.createScreenCapture(new Rectangle(0, 0, 400, 300));
+    	
+    	ImageFrame frame = new ImageFrame(image);
+    	frame.setVisible(true);
+    }
 }
 
 
