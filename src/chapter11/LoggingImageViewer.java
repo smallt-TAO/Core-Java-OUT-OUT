fac72ec618059da2bc1c81506f8bc323e5af96b6
smallt-TAO <llt5662720@163.com>
2017-02-16 18:34:12 +0800

    #20170216 Today update the LoggingImage viewer.

---
+++ src/chapter11/LoggingImageViewer.java
@@ -0,0 +1,63 @@
+package chapter11;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.logging.*;
+import javax.swing.*;
+
+/**
+ * A modification of the image viewer program that logs various events.
+ * @author Smalltao
+ *
+ */
+
+public class LoggingImageViewer {
+
+}
+
+/**
+ * The frame that shows the image.
+ */
+class ImageViewerFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel label;
+	private static Logger logger = Logger.getLogger("com.horstmann.corejava");
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 400;  
+	
+}
+
+
+
+/**
+ * A handler for displaying log records in a window.
+ */
+class WindowHandler extends StreamHandler {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
