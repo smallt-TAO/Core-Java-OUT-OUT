1ac07ba269139716081620db41cc6931698a1daa
smallt-TAO <llt5662720@163.com>
2017-02-17 19:24:19 +0800

    #20170217 Today updates the log again.

--- src/chapter11/LoggingImageViewer.java
+++ src/chapter11/LoggingImageViewer.java
@@ -30,6 +30,20 @@ class ImageViewerFrame extends JFrame {
 	private static final int DEFAULT_WIDTH = 300;
 	private static final int DEFAULT_HEIGHT = 400;  
 	
+	public ImageViewerFrame() {
+		logger.entering("ImageViewerFrame", "<init>");
+		setTitle("LoggingImageViewer");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		// set up menu bar
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu menu = new JMenu("File");
+		menuBar.add(menu);
+		
+		
+	}
 }
 
 
@@ -38,26 +52,30 @@ class ImageViewerFrame extends JFrame {
  * A handler for displaying log records in a window.
  */
 class WindowHandler extends StreamHandler {
+	private JFrame frame;
 	
+	public WindowHandler() {
+		frame = new JFrame();
+		final JTextArea output = new JTextArea();
+		output.setEditable(false);
+		frame.setSize(200, 200);
+		frame.add(new JScrollPane(output));
+		frame.setFocusableWindowState(false);
+		frame.setVisible(true);
+		setOutputStream(new OutputStream() {
+			public void write(int b) {
+				
+			}
+			
+			public void write(byte[] b, int off, int len) {
+				output.append(new String(b, off, len));
+			}
+		});
+	}
+	
+	public void publish(LogRecord record) {
+		if (!frame.isVisible()) return;
+		super.publish(record);
+		flush();
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
