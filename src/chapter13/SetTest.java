8cc25addfd7d8bd2cbd47ab178e63dc674e9fa2c
smallt-TAO <llt5662720@163.com>
2017-02-24 14:10:11 +0800

    #20170224 Come on. Smalltao.

---
+++ src/chapter13/SetTest.java
@@ -0,0 +1,31 @@
+package chapter13;
+
+import java.util.*;
+
+/**
+ * This program uses a set to print all unique words in System.in.
+ * @author Smalltao
+ *
+ */
+public class SetTest {
+    public static void main(String[] args) {
+    	Set<String> words = new HashSet<String>();
+    	long totalTime = 0;
+    	
+    	Scanner in = new Scanner(System.in);
+    	while (in.hasNext()) {
+    		String word = in.next();
+    		long callTime = System.currentTimeMillis();
+    		words.add(word);
+    		callTime = System.currentTimeMillis() - callTime;
+    		totalTime += callTime;
+    	}
+    	in.close();
+    	
+    	Iterator<String> iter = words.iterator();
+    	for (int i = 1; i <= 20 && iter.hasNext(); i++) 
+    		System.out.println(iter.next());
+    	System.out.println(". . .");
+    	System.out.println(words.size() + " distinct words." + totalTime + " milliseconds.");
+    }
+}
